a9186bfd83e25560ba21582bf3c0c6035e1afd00
Daniel <danielvwilkinson@gmail.com>
2020-06-30 23:12:42 +0100

    added styling to complete mobile at 320px added JS for slider and input

--- index.js
+++ index.js
@@ -1,36 +1,55 @@
-//ACCORDION ---------------------------------------------------------------
+//Accordion ---------------------------------------------------------------
 
-let headerArray = document.querySelectorAll(".accordion-header");
+const headerArray = document.querySelectorAll(".accordion-header");
 
 for (let i = 0; i < headerArray.length; i++) {
-  let accordHeader = document.getElementById(`accordion-header-${i}`);
-  let accordBody = document.getElementById(`body-${i}`);
+  const accordHeader = document.getElementById(`accordion-header-${i}`);
+  const accordBody = document.getElementById(`body-${i}`);
 
-  let toggleAccordion = () => {
+  function toggleAccordion() {
     accordBody.classList.toggle("hide");
     accordHeader.classList.toggle("active");
-  };
+  }
 
   accordHeader.addEventListener("click", toggleAccordion);
 }
 
+//restrict input min and max on the borrow field -----------------------
 
+const borrowInput = document.getElementById("borrow");
+const slider = document.getElementById("range");
 
-//restrict input min and max on the borrow field
-
-function minMax(el){
-    console.log(el.min);
-    if(el.value != ""){
-      if(parseInt(el.value) < parseInt(el.min)){
-        el.value = el.min;
-      }
-      if(parseInt(el.value) > parseInt(el.max)){
-        el.value = el.max;
-      }
+function minMax() {
+  if (borrowInput.value != "") {
+    if (parseInt(borrowInput.value) < parseInt(borrowInput.min)) {
+      borrowInput.value = borrowInput.min;
+    }
+    if (parseInt(borrowInput.value) > parseInt(borrowInput.max)) {
+      borrowInput.value = borrowInput.max;
     }
   }
-  
-let borrowInput = document.getElementById("borrow");
-borrowInput.addEventListener("blur", function() {
-    minMax(borrowInput);
+}
+
+borrowInput.addEventListener("blur", function () {
+  minMax();
+});
+
+//change range on type in borrow input --------------------------------
+function changeRange() {
+  slider.value = borrowInput.value;
+  if (borrowInput.value === "") {
+    slider.value = "100";
+  }
+}
+
+borrowInput.addEventListener("keyup", changeRange);
+
+//on change of slider change the input field --------------------------
+
+function changeNumberInput() {
+  borrowInput.value = slider.value;
+}
+
+slider.addEventListener("input", function () {
+  changeNumberInput();
 });
